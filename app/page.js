0e67264dc98cce059a8790fa7caa1eebe0239a0c
jesusvaz <jesusvaz@gmail.com>
2024-03-16 11:39:36 -0600

    Delete possible scape problemss

--- app/page.js
+++ app/page.js
@@ -26,9 +26,9 @@ export default function Home() {
         rel="noopener noreferrer"
       >
         <h2 className={`mb-3 text-2xl font-semibold`}>
-          Nail Polish{' '}
+          Nail Polish
           <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-            -&gt;
+         
           </span>
         </h2>
         <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
@@ -43,9 +43,9 @@ export default function Home() {
         rel="noopener noreferrer"
       >
         <h2 className={`mb-3 text-2xl font-semibold`}>
-          Nail Repair {' '}
+          Nail Repair 
           <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-            -&gt;
+            
           </span>
         </h2>
         <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
@@ -59,9 +59,9 @@ export default function Home() {
         rel="noopener noreferrer"
       >
         <h2 className={`mb-3 text-2xl font-semibold`}>
-          Treatments {' '}
+          Treatments 
           <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-            -&gt;
+            
           </span>
         </h2>
         <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
@@ -76,9 +76,9 @@ export default function Home() {
         rel="noopener noreferrer"
       >
         <h2 className={`mb-3 text-2xl font-semibold`}>
-          Manicure & Pedicure{' '}
+          Manicure & Pedicure
           <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-            -&gt;
+            
           </span>
         </h2>
         <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
@@ -91,9 +91,9 @@ export default function Home() {
         rel="noopener noreferrer"
       >
         <h2 className={`mb-3 text-2xl font-semibold`}>
-          Gels{' '}
+          Gels
           <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-            -&gt;
+            
           </span>
         </h2>
         <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
@@ -106,9 +106,9 @@ export default function Home() {
         rel="noopener noreferrer"
       >
         <h2 className={`mb-3 text-2xl font-semibold`}>
-          Wraps{' '}
+          Wraps
           <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-            -&gt;
+            
           </span>
         </h2>
         <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
